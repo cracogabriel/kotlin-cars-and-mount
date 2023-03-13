57da3e12965fb62dd32cb46e998883c161b42a50
craco <gabriel_craco@hotmail.com>
2023-03-13 11:02:02 -0300

    fisrt commit!

---
+++ src/main/kotlin/com/cracodev/carsandmount/CarsAndMountApplication.kt
@@ -0,0 +1,11 @@
+package com.cracodev.carsandmount
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarsAndMountApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarsAndMountApplication>(*args)
+}
---
+++ src/test/kotlin/com/cracodev/carsandmount/CarsAndMountApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cracodev.carsandmount
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarsAndMountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
